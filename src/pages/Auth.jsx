7b7a002f01c1016cfdeb7c25aa1940baa58f9e36
fxruq <temiladefaruq2006@gmail.com>
2026-05-09 03:40:14 +0100

    added home page

---
+++ src/pages/Auth.jsx
@@ -0,0 +1,11 @@
+
+
+const Auth = () => {
+  return (
+    <div>
+      auth page
+    </div>
+  )
+}
+
+export default Auth
---
+++ src/pages/Checkout.jsx
@@ -0,0 +1,10 @@
+
+const Checkout = () => {
+  return (
+    <div>
+      checkout page
+    </div>
+  )
+}
+
+export default Checkout
